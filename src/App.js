36543be57a685d7199c0968deeb6f79f6d30a41c
eli-zhang <zeliboid8@gmail.com>
2022-07-13 14:09:19 -0400

    Add home page grid

--- src/App.js
+++ src/App.js
@@ -1,10 +1,31 @@
 import logo from './logo.svg';
-import { Title, NavLink } from './styled'
+import { Title, RoomGrid, RoomItem, RoomImage, RoomTitle } from './styled'
 
 function App() {
   return (
     <div className="App">
       <Title>301 Dryden Road</Title>
+      <RoomGrid>
+        <RoomItem row={1} col={1}>
+          <RoomImage />
+          <RoomTitle>Room 101</RoomTitle>
+        </RoomItem>
+        <RoomItem row={1} col={2}/>
+        <RoomItem row={1} col={3}/>
+        <RoomItem row={1} col={4}/>
+        <RoomItem row={2} col={1}/>
+        <RoomItem row={2} col={2}/>
+        <RoomItem row={2} col={3}/>
+        <RoomItem row={2} col={4}/>
+        <RoomItem row={3} col={1}/>
+        <RoomItem row={3} col={2}/>
+        <RoomItem row={3} col={3}/>
+        <RoomItem row={3} col={4}/>
+        <RoomItem row={4} col={1}/>
+        <RoomItem row={4} col={2}/>
+        <RoomItem row={4} col={3}/>
+        <RoomItem row={4} col={4}/>
+      </RoomGrid>
     </div>
   );
 }
--- src/styled.js
+++ src/styled.js
@@ -1,6 +1,38 @@
 import styled from 'styled-components';
+import room1 from './images/room1.jpg';
 
 export const Title = styled.h1`
     color: #222222;
     font-size: 40px;
+`
+
+export const RoomGrid = styled.div`
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    column-gap: 10px;
+    grid-template-rows: auto;
+    height: 400px;
+    row-gap: 10px;
+    place-items: stretch;
+    background: #F1F1F6;
+`
+
+export const RoomItem = styled.div`
+    grid-column-start: ${props => props.col};
+    grid-row-start: ${props => props.row};
+    background: white;
+    grid-column-end: span 1;
+    grid-row-end: span 1;
+`
+
+export const RoomImage = styled.div`
+    width: 100%;
+    height: 70%;
+    background-image: url(${room1});
+    background-repeat:no-repeat;
+    background-size:cover;
+`
+
+export const RoomTitle = styled.p`
+    color: black;
 `
